6748bcdba7692eac72ffd6f0314af21300b67791
Jarmo Lindstrom <yaramodesu@gmail.com>
2021-01-06 15:00:01 +0200

    loma ohi, stylet tehty ja ihan pihalla

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const path = require('path')
 const mongoose = require('mongoose')
+const ejsMate = require('ejs-mate')
 const Campground = require('./models/campground')
+const methodOverride = require('method-override')
 
 const app = express()
 
@@ -17,10 +19,12 @@ db.once("open", () => {
     console.log('database yhdistetty')
 })
 
+app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.use(express.urlencoded({ extended: true}))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -50,6 +54,20 @@ app.get('/campgrounds/:id/edit', async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/edit', { campground })
 }) 
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params
+    
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground})
+    
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params  
+    await Campground.findByIdAndDelete(id)
+    res.redirect('/campgrounds')  
+})
 
 
 
