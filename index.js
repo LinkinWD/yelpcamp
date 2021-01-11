164f80c296de3f24f0c18f6b5afb49f02401709e
Jarmo Lindstrom <yaramodesu@gmail.com>
2021-01-11 12:18:54 +0200

    väli seivaus, kommentit edistyy onnisstuneesti

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const catchAsync = require('./utilities/catchAsync')
 const ExpressError = require('./utilities/expressError')
 const Campground = require('./models/campground')
 const methodOverride = require('method-override')
+const Review = require('./models/rewiev')
 
 const app = express()
 
@@ -86,6 +87,17 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params  
     await Campground.findByIdAndDelete(id)
     res.redirect('/campgrounds')  
+
+}))
+
+//arvostelut
+app.post('/campgrounds/:id/reviews', catchAsync(async(req, res) => {
+    const campground = await Campground.findById(req.params.id)
+    const review = new Review(req.body.review)
+    campground.reviews.push(review)
+    await review.save()
+    await campground.save()
+    res.redirect(`/campgrounds/${campground._id}`)
 }))
 
 app.all('*', (req, res, next) => {
