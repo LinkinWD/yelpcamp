72ba52ab03891b179ee277fdb4aa6180a05a3d45
Jarmo Lindstrom <yaramodesu@gmail.com>
2021-01-20 04:07:50 +0200

    controllerit lisätty ja pieni maistiainen miten paremmin tekeee routet

--- routes/user.js
+++ routes/user.js
@@ -4,46 +4,19 @@ const { nextTick } = require('process')
 const router = express.Router()
 const User = require('../models/user')
 const catchAsync = require('../utilities/catchAsync')
+const users = require('../controllers/users')
 
-router.get('/register', (req, res) => {
-    res.render('users/register')
-})
-router.post('/register', catchAsync(async(req, res, next) => {
-    try {
-    const {email, username, password} = req.body
-    const user = new User({email, username})
-    const registerUser = await User.register(user, password)
-    req.login(registerUser, err => {
-        if(err) return next(err)
-        req.flash('success', 'welcome to yelpcamp new user')
-        res.redirect('/campgrounds')
-    })
-    
-    } catch(e) {
-        req.flash('error', e.message)
-        res.redirect('register')
-    }
-    
-}))
+router.get('/register', users.registerForm)
 
-router.get('/login', (req, res) => {
-    res.render('users/login')
-})
+router.post('/register', catchAsync(users.register))
+
+router.get('/login', users.loginForm)
 
 router.post('/login', passport.authenticate('local', {
     failureFlash: true,
     failureRedirect: '/login'
-}), (req, res) => {
-    req.flash('success', 'Welcome back')
-    const redirectUrl = req.session.returnTo || '/campgrounds'
-    delete req.session.returnTo
-    res.redirect(redirectUrl)
-})
+}), users.login)
 
-router.get('/logout', (req, res) => {
-    req.logout()
-    req.flash('success', 'logged you out')
-    res.redirect('/campgrounds')
-})
+router.get('/logout', users.logout)
 
 module.exports = router
